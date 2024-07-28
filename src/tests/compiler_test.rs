c13a359394233f1ed1b8fb3d632887b981524904
y_mc <momo.toure27@gmail.com>
2024-07-28 18:25:14 +0200

    bug fix and  add  test

--- src/tests/compiler_test.rs
+++ src/tests/compiler_test.rs
@@ -7,8 +7,10 @@ mod tests {
     #[test]
     pub fn test_lexer() {
         use ymcrust::lexxer;
-        let result = lexxer("2 + 2");
-        assert_eq!(result, vec!["2", "+", "2"]);
+        use ymcrust::Token::{Number, Plus};
+        let source_code = "5 + 7   12  ";
+        let result = lexxer(source_code);
+        assert_eq!(result, vec![Number(5), Plus, Number(7), Number(12)]);
     }
 
 
